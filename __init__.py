91938542a97f84978c0f12d84727a6fa720655a6
Jannik Smidt <js343@hdm-stuttgart.de>
2020-08-03 14:10:22 +0200

    bugfix

--- __init__.py
+++ __init__.py
@@ -246,7 +246,7 @@ class Appointment(MycroftSkill):
         spoken_date = None
         try:
             spoken_date = extract_datetime(message, start, self.lang)
-        except ValueError:
+        except AttributeError:
             pass
         while spoken_date is None:
             try:
